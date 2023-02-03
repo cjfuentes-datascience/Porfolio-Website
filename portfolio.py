c2b7ee58c035ba0a773cbb94e9c0d524344e3f4d
Carlos Fuentes <104155059+cjfuentes-datascience@users.noreply.github.com>
2023-02-03 10:59:46 -0500

    Add files via upload

---
+++ portfolio.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import yfinance as yf
+
+st.title('Carlos J. Fuentes: Data Scientist :panda_face:')
+st.write('''Hi, I'm Carlos! :wave: In this web app, you'll find a collection of projects I've build using various different programming tools.   
+
+''')
+st.write("Simply follow the instructions below to get a glimpse of what I've built throughout my career. :izakaya_lantern:")
+st.write('You can use the side bar to navigate the different pages in this webapp. :world_map:')
+st.write('Hope you enjoy! :smile:')
+st.write('---')
+
+# building the sidebar
+with st.sidebar:
+    st.title('Project Portfolio')
+    choice = st.radio('Navigation', ['Streamlit', 'Python', 'SQL', 'PowerBI', 'Tableau'])
+    st.info("This sidebar allows you to navigate to various portfolio destinations in order to view some of the applications and dashboards I've developed. :100:")
+
+# making the sidebar selections work
+if choice=='Streamlit':
+    st.subheader('Streamlit Projects :spider_web:')
+    st.write("Click the button below to view a list of **Streamlit** applications I've developed to bring data to life.")
+    link = 'https://github.com/cjfuentes-datascience/STREAMLIT-PROJECTS'
+    st.button('Github', link)
+
+if choice=='Python':
+    st.subheader('Python Projects :snake:')
+    st.write("Click the button below to view a list of **Python** applications I've developed to bring data to life.")
+    link = 'https://github.com/cjfuentes-datascience/PYTHON-PROJECTS'
+    st.button('Github', link)
+
+if choice=='SQL':
+    st.subheader('SQL Projects :bookmark_tabs:')
+    st.write("Click the button below to view a list of **SQL** applications I've developed to bring data to life.")
+    link = 'https://github.com/cjfuentes-datascience/SQL-PROJECTS'
+    st.button('Github', link)
+
+if choice=='PowerBI':
+    st.subheader('PowerBI Projects :chart_with_upwards_trend:')
+    st.write("Click the button below to view a list of **PowerBI** applications I've developed to bring data to life.")
+    link = 'https://github.com/cjfuentes-datascience/POWER-BI-PROJECTS'
+    st.button('Github', link)
+
+if choice=='Tableau':
+    st.subheader('Tableau Projects :chart_with_downwards_trend:')
+    st.write("Click the button below to view a list of **Tableau** applications I've developed to bring data to life.")
+    link = 'https://github.com/cjfuentes-datascience/TABLEAU-PROJECTS'
+    st.button('Github', link)
+
+st.write('---')
+
+st.text('Thanks For Stopping By!')
