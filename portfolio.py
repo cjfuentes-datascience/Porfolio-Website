b3f286f9ba89b5e29bd177f5a9b128c00581da47
Carlos Fuentes <104155059+cjfuentes-datascience@users.noreply.github.com>
2023-02-06 21:15:23 -0500

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -28,7 +28,7 @@ if choice=='Python':
     st.markdown(f'<a href="{link}" target="_blank" style="background-color: #4681f4; padding: 8px 16px; font-size: 16px; color: white; border-radius: 4px;">Github</a>', unsafe_allow_html=True)
 
 if choice=='Streamlit':
-    st.subheader('Streamlit Projects :spider_web:')
+    st.subheader('Streamlit Projects :spider: :spider_web:')
     st.write("Click the button below to view a list of **Streamlit** applications I've developed to bring data to life.")
     link = 'https://github.com/cjfuentes-datascience/STREAMLIT-PROJECTS'
     st.markdown(f'<a href="{link}" target="_blank" style="background-color: #4681f4; padding: 8px 16px; font-size: 16px; color: white; border-radius: 4px;">Github</a>', unsafe_allow_html=True)
