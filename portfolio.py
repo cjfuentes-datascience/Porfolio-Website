6ae9941dd56c5539a4e9ee9c2b888c7b197b241c
Carlos Fuentes <104155059+cjfuentes-datascience@users.noreply.github.com>
2023-02-25 11:45:06 -0500

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -170,20 +170,19 @@ if choice=='PowerBI':
     st.write("""
         Below is a collection of Power BI projects I've compiled and uploaded to GitHub. Please feel free to have a look!
 
-        - Sales-Report: The report includes several visualizations and a dashboard that allows users to explore and analyze different aspects of the sales data. https://github.com/cjfuentes-datascience/Sales-Report-PowerBI
+        - Sales-Report: https://github.com/cjfuentes-datascience/Sales-Report-PowerBI
 
-        - Data-Professional-Survey: This project involves analyzing data from a survey of data professionals, including their demographics, skills, and experience, and presenting the results in a Power BI dashboard. https://github.com/cjfuentes-datascience/Data-Professional-Survey
+        - Data-Professional-Survey: https://github.com/cjfuentes-datascience/Data-Professional-Survey
 
-        - Executive-Summary-Finance-Report: This project involves creating an executive summary of financial data and presenting it in a Power BI dashboard. https://github.com/cjfuentes-datascience/Executive-Summary-Finance-Report 
+        - Executive-Summary-Finance-Report: https://github.com/cjfuentes-datascience/Executive-Summary-Finance-Report 
 
-        - Exploring-Top-Google-Searches-2021: This project involves analyzing data on the top Google searches of 2021 and presenting the results in a Power BI dashboard.
-        https://github.com/cjfuentes-datascience/Exploring-Top-Google-Searches-2021-PowerBI
+        - Exploring-Top-Google-Searches-2021: https://github.com/cjfuentes-datascience/Exploring-Top-Google-Searches-2021-PowerBI
 
-        - E-Commerce-Data-and-Profit-Analysis: This project involves analyzing data on an e-commerce business, including sales data and customer demographics, and presenting the results in a Power BI dashboard. https://github.com/cjfuentes-datascience/E-Commerce-Data-and-Profit-Analysis
+        - E-Commerce-Data-and-Profit-Analysis: https://github.com/cjfuentes-datascience/E-Commerce-Data-and-Profit-Analysis
 
-        - Adventure-Works-Sales-Analysis: This project involves analyzing sales data for a fictional company, Adventure Works, and presenting the results in a Power BI dashboard. https://github.com/cjfuentes-datascience/Adventure-Works-Sales-Analysis
+        - Adventure-Works-Sales-Analysis: https://github.com/cjfuentes-datascience/Adventure-Works-Sales-Analysis
 
-        - IT-Spend: This project involves analyzing data on IT spend and presenting it in a Power BI dashboard. https://github.com/cjfuentes-datascience/IT-Spend
+        - IT-Spend: https://github.com/cjfuentes-datascience/IT-Spend
 """)
 
 if choice=='Tableau':
