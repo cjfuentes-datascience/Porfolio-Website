dd668e5e505c012c2a12ebc0c775c4afc650a2ec
Carlos Fuentes <104155059+cjfuentes-datascience@users.noreply.github.com>
2023-02-25 11:43:38 -0500

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -137,35 +137,25 @@ if choice=='SQL':
     st.write("""
         Below is a collection of SQL projects I've compiled and uploaded to GitHub. Please feel free to have a look!
 
-        - SpeedySpoon Data Aggregation: A project involving the aggregation of data from the SpeedySpoon database.
-        https://github.com/cjfuentes-datascience/SpeedySpoon-Data-Aggregation-SQL
+        - SpeedySpoon Data Aggregation: https://github.com/cjfuentes-datascience/SpeedySpoon-Data-Aggregation-SQL
 
-        - Business Metrics For Mineblocks: A project analyzing business metrics for the Mineblocks game.
-        https://github.com/cjfuentes-datascience/Business-Metrics-For-Mineblocks-SQL
+        - Business Metrics For Mineblocks: https://github.com/cjfuentes-datascience/Business-Metrics-For-Mineblocks-SQL
 
-        - Bakery Functions: A project involving the creation of functions for a bakery database.
-        https://github.com/cjfuentes-datascience/Bakery-Functions-SQL
+        - Bakery Functions: https://github.com/cjfuentes-datascience/Bakery-Functions-SQL
 
-        - Airline Table Transformations: A project involving the transformation of tables in an airline database.
-        https://github.com/cjfuentes-datascience/Airline-Table-Transformations-SQL
+        - Airline Table Transformations: https://github.com/cjfuentes-datascience/Airline-Table-Transformations-SQL
 
-        - Transforming Flight Tables: A project involving the transformation of flight tables.
-        https://github.com/cjfuentes-datascience/Tranforming-Flight-Tables-SQL
+        - Transforming Flight Tables: https://github.com/cjfuentes-datascience/Tranforming-Flight-Tables-SQL
 
-        - Set Operations: A project involving the use of set operations in SQL.
-        https://github.com/cjfuentes-datascience/Set-Operations-SQL
+        - Set Operations: https://github.com/cjfuentes-datascience/Set-Operations-SQL
 
-        - Customer Segmentation: A project involving the segmentation of customers in a database.
-        https://github.com/cjfuentes-datascience/Customer-Segmentation-SQL
+        - Customer Segmentation: https://github.com/cjfuentes-datascience/Customer-Segmentation-SQL
 
-        - New York Restaurants: A project involving the analysis of restaurant data in New York. 
-        https://github.com/cjfuentes-datascience/New-York-Restaurants-SQL
+        - New York Restaurants: https://github.com/cjfuentes-datascience/New-York-Restaurants-SQL
 
-        - Fraud Detection: A project using SQL to detect fraudulent activity.
-        https://github.com/cjfuentes-datascience/Fraud-Detection-Python
+        - Fraud Detection: https://github.com/cjfuentes-datascience/Fraud-Detection-Python
 
-        - Analyze Hacker News Trends: A project analyzing trends in Hacker News data using SQL.
-        https://github.com/cjfuentes-datascience/Analyze-Hacker-News-Trends-SQL
+        - Analyze Hacker News Trends: https://github.com/cjfuentes-datascience/Analyze-Hacker-News-Trends-SQL
 """)
 
 if choice=='PowerBI':
