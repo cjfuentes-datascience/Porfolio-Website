c2f3f84b00038d673880fd30a6ca616abc431a37
Carlos Fuentes <104155059+cjfuentes-datascience@users.noreply.github.com>
2023-02-25 18:13:05 -0500

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -48,7 +48,7 @@ if choice=='Streamlit':
     
     st.write('---')
     st.subheader("Github Portfolio")
-    st.write("Click the button below to view a list of **Streamlit** applications in Github.")
+    st.write("Click the button below to view a list of **Streamlit** applications in Github. :point-down:")
     link = 'https://github.com/cjfuentes-datascience/STREAMLIT-PROJECTS'
     st.markdown(f'<a href="{link}" target="_blank" style="background-color: #4681f4; padding: 8px 16px; font-size: 16px; color: white; border-radius: 4px;">Github</a>', unsafe_allow_html=True)
 
